780031cbcb8b129c84e61201050d4c539d2d7586
greengod540 <72803672+greengod540@users.noreply.github.com>
2023-11-19 20:54:16 +0200

    Delete First SDL SetUp directory
    2E2EQ

--- First SDL SetUp/TextureLoader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <SDL.h>
-#include <iostream>
-#include <filesystem>
-
-class TextureLoader {
-public:
-    SDL_Texture* CreateTexture(std::string imagePathBMP, SDL_Renderer* renderer);
-};
